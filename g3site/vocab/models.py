51f984211025be3d1f527721ff4658474ea4c2a0
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-02-21 21:34:05 +0700

    Flush db and can see word

--- g3site/vocab/models.py
+++ g3site/vocab/models.py
@@ -4,20 +4,13 @@ from django.utils import timezone
 
 class Word(models.Model):
     word_text = models.CharField(max_length=50)
-    add_date = models.DateTimeField('date vocab added')
     def __str__(self):
         return self.word_text
 
-    def add_date(self):
-        return self.add_date
     
 class Mean(models.Model):
     mean_text = models.CharField(max_length=200)
     type_text = models.CharField(max_length=10)
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    add_date = models.DateTimeField('date vocab added')
     def __str__(self):
         return self.mean_text
-        
-    def add_date(self):
-        return self.add_date
--- g3site/vocab/views.py
+++ g3site/vocab/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import Word ,Mean
+from django.utils import timezone
 
-from django.http import HttpResponse
 
+class IndexView(generic.ListView):
+    template_name = 'vocab/index.html'
+    context_object_name = 'word_list'
+           
+    def get_queryset(self):
+        return Word.objects.all()
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+class DetailView(generic.DetailView):#เมื่อมีการ request จากการกด Question จากหน้า index.html จะทำการเปิด detail.html
+    model = Word
+    template_name = 'vocab/detail.html'
+    def get_queryset(self):
+        return Word.objects.all()
